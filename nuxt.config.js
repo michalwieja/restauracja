b984fcd2af15ac193adbce255de6d9223006fe7e
mwConceptX <mwieja@yahoo.com>
2020-11-11 12:02:57 +0100

    config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,10 +1,10 @@
 export default {
   // Target (https://go.nuxtjs.dev/config-target)
   target: "static",
-  server: {
-    port: 8000, // default: 3000
-    host: "0.0.0.0" // default: localhost
-  },
+  // server: {
+  //   port: 8000, // default: 3000
+  //   host: "0.0.0.0" // default: localhost
+  // },
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
